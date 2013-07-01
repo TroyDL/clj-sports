bb2aac027718636215164c0b49f9388241d2b241
troydl <troydl@gmail.com>
2013-07-01 09:51:28 -0700

    Defined initial import routines from sql to neo4j.

--- src/user.clj
+++ src/user.clj
@@ -15,6 +15,8 @@
               :user "root"
               :password "root"}))
 
+(nr/connect! "http://localhost:7474/db/data/")
+
 (declare
  sql-player                      sql-coach
  sql-action                      sql-action-type
@@ -182,11 +184,212 @@
   (kc/belongs-to sql-season)
   (kc/belongs-to sql-league))
 
+(def index-map {:sports_idx "name"
+                :positions_idx "name"
+                :coaches_idx "id"})
+(defn query
+  ([index-name]
+     (pprint
+      (map
+       #(get-in % ["n" :data])
+       (cy/tquery
+        (str "start n = node:" index-name "('"
+             (get index-map index-name) ":*') return n limit 10")))))
+  #_([index-name index-key return-keys]
+       (cy/tquery (str "start n = node:" index-name "('"
+                       index-key ":*') return" return-keys))))
+
+(defn create-unique-index [name]
+  (try (nn/create-index name {:unique true})
+       (catch Exception e)))
+
+(defn import-sports []
+  (try (nn/create-index "sports_idx"
+                           {:unique true})
+          (catch Exception e 
+            "borked, probably already exists"))
+  (let [sports-seq (kc/select sql-sport)]
+    (doseq [sport sports-seq]
+      (nn/create-unique-in-index "sports_idx"
+                                 :name
+                                 (:name sport)
+                                 sport))))
+
+(defn import-positions []
+  (try (nn/create-index "positions_idx"
+                        {:unique true})
+       (catch Exception e "exists already"))
+  (let [positions-seq (kc/select sql-position)
+        {sport-id :id} (first (nn/query "sports_idx" "name:Football"))]
+    (doseq [pos positions-seq]
+      (let [{pos-name :position_name} pos
+            pos-id (get-in (nn/create-unique-in-index
+                            "positions_idx"
+                            :name pos-name pos)
+                           [:id])]
+        (do (nrl/create sport-id
+                        pos-id 
+                        "has_position"))))))
+
+(defn import-coaches []
+  (create-unique-index "coaches_idx")
+  (let [coaches-seq (kc/select sql-coach
+                      (kc/fields :id :firstname :lastname :slug))]
+    (doseq [coach coaches-seq]
+      (nn/create-unique-in-index "coaches_idx"
+                                 :id
+                                 (:id coach)
+                                 coach))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#_(kc/defentity sql-sport
+    sql-position
+(kc/defentity sql-coach
+(kc/defentity sql-game-type
+(kc/defentity sql-player
+(kc/defentity sql-school
+(kc/defentity sql-score-type
+(kc/defentity sql-season
+(kc/defentity sql-stat-type ---
+(kc/defentity sql-team
+(kc/defentity sql-game
+(kc/defentity sql-action-type
+(kc/defentity sql-action
+(kc/defentity sql-action-type-score-type
+(kc/defentity sql-coach-team-season
+(kc/defentity sql-league
+(kc/defentity sql-league-season
+(kc/defentity sql-matchup
+(kc/defentity sql-player-team-season
+(kc/defentity sql-player-team-season-position
+(kc/defentity sql-score
+(kc/defentity sql-stat
+(kc/defentity sql-team-season))))))))))))))))))))))
+
+#_((kc/defentity sql-sport)
+  (kc/defentity sql-coach
+    (kc/has-many sql-coach-team-season))
+  (kc/defentity sql-game-type
+    (kc/has-many sql-game))
+  (kc/defentity sql-player
+    (kc/has-many sql-stat))
+  (kc/defentity sql-school
+    (kc/has-many sql-game)
+    (kc/has-one sql-team))
+  (kc/defentity sql-score-type
+    (kc/has-many sql-action)
+    (kc/has-many sql-action-type-score-type))
+  (kc/defentity sql-season
+    (kc/belongs-to sql-sport)
+    (kc/has-many sql-coach-team-season)
+    (kc/has-many sql-game)
+    (kc/has-many sql-league-season)
+    (kc/has-many sql-team-season))
+  (kc/defentity sql-stat-type
+    (kc/belongs-to sql-sport)
+    (kc/has-many sql-stat))
+  (kc/defentity sql-team
+    (kc/belongs-to sql-school)
+    (kc/belongs-to sql-sport)
+    (kc/has-many sql-action)
+    (kc/has-many sql-coach-team-season)
+    (kc/has-many sql-matchup)
+    (kc/has-many sql-score)
+    (kc/has-many sql-stat)
+    (kc/has-many sql-team-season))
+  (kc/defentity sql-game
+    (kc/belongs-to sql-season)
+    (kc/belongs-to sql-sport)
+    (kc/belongs-to sql-game-type)
+    (kc/belongs-to sql-school)
+    (kc/has-many sql-action)
+    (kc/has-many sql-score)
+    (kc/has-many sql-stat)
+    (kc/has-one sql-matchup))
+  (kc/defentity sql-action-type
+    (kc/belongs-to sql-sport)
+    (kc/has-many sql-action)
+    (kc/has-many sql-action-type-score-type))
+  (kc/defentity sql-action
+    (kc/belongs-to sql-game)
+    (kc/belongs-to sql-team)
+    (kc/belongs-to sql-action-type)
+    (kc/belongs-to sql-score-type))
+  (kc/defentity sql-action-type-score-type
+    (kc/belongs-to sql-action-type)
+    (kc/belongs-to sql-score-type))
+  (kc/defentity sql-coach-team-season
+    (kc/belongs-to sql-coach)
+    (kc/belongs-to sql-team))
+  (kc/defentity sql-league
+    (kc/belongs-to sql-sport)
+    (kc/has-many sql-league-season)
+    (kc/has-many sql-team-season))
+  (kc/defentity sql-league-season
+    (kc/belongs-to sql-league)
+    (kc/belongs-to sql-season))
+  (kc/defentity sql-matchup
+    (kc/belongs-to sql-game)
+    (kc/belongs-to sql-team))
+  (kc/defentity sql-player-team-season
+    (kc/belongs-to sql-player)
+    (kc/belongs-to sql-team)
+    (kc/belongs-to sql-season))
+  (kc/defentity sql-player-team-season-position
+    (kc/belongs-to sql-player-team-season)
+    (kc/belongs-to sql-position))
+  (kc/defentity sql-score
+    (kc/belongs-to sql-game)
+    (kc/belongs-to sql-team))
+  (kc/defentity sql-stat
+    (kc/belongs-to sql-stat-type)
+    (kc/belongs-to sql-game)
+    (kc/belongs-to sql-player)
+    (kc/belongs-to sql-team))
+  (kc/defentity sql-team-season
+    (kc/belongs-to sql-team)
+    (kc/belongs-to sql-season)
+    (kc/belongs-to sql-league)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #_(kc/select sql-player)
 
-#_(nr/connect! "http://localhost:7474/db/data/")
 
 
 
